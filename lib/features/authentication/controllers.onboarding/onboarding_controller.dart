2818089e0e738de1e294295f4da61f2a945173b8
Adeoye John Jaycrown <Jaycrown9119@gmail.com>
2025-09-25 21:05:23 +0100

    update

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+static OnBoardingController get instance => Get.find();
+
+
+/// Variables
+final pageController = PageController();
+Rx<int> currentPageIndex = 0.obs;
+
+/// Update Current Index when page Scroll
+void updatePageIndicator(index) => currentPageIndex = index;
+
+/// Jump to the specific dot selected page.
+void dotNavigationClick(index){
+  currentPageIndex.value = index;
+  pageController.jumpToPage(index);
+}
+
+///Update Current Index to next page
+void nextPage() {
+  if (currentPageIndex.value == 2){
+ //   Get.to(LoginScreen());
+  }else {
+    int page = currentPageIndex.value + 1;
+    pageController.jumpToPage(page);
+  }
+}
+
+/// Update Current Index & Jump to the last page
+ void skipPage() {
+  currentPageIndex.value = 2;
+  pageController.jumpToPage(2);
+ }
+}
